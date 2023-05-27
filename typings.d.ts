39ff06f54a19a79f1505292fd21dc5bffbe4e2dc
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-05-27 17:50:31 +0000

    ⚡️ stepzen updates

--- typings.d.ts
+++ typings.d.ts
@@ -8,12 +8,28 @@ interface CurrentWeather {
 }
 
 interface Daily {
+	apparent_temperature_max: [number];
+	apparent_temperature_min: [number];
+	sunrise: [string];
+	sunset: [string];
+	temperature_2m_max: [number];
+	temperature_2m_min: [number];
 	time: [string];
+	uv_index_clear_sky_max: [number];
+	uv_index_max: [number];
 	weathercode: [number];
 }
 
 interface DailyUnits {
+	apparent_temperature_max: string;
+	apparent_temperature_min: string;
+	sunrise: string;
+	sunset: string;
+	temperature_2m_max: string;
+	temperature_2m_min: string;
 	time: string;
+	uv_index_clear_sky_max: string;
+	uv_index_max: string;
 	weathercode: string;
 }
 
